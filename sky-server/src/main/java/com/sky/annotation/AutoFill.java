4d223a26e859280517f863e90a550ed660d4e95d
O.Dentist <1983510227@QQ.COM>
2023-04-18 11:21:14 +0800

    commit Annotation autoFill

---
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -0,0 +1,14 @@
+package com.sky.annotation;
+
+import com.sky.enumeration.OperationType;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+    public @interface AutoFill {
+        //数据库操作类型：UPDATE INSERT
+        OperationType value();
+    }
